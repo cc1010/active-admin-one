d8daf8e80310e5b37b4bf361b9c5f8c6bdb8d5b1
Chaim Chazan <chaimyisroel@gmail.com>
2021-11-04 22:52:05 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # resources :ingredients
+  # resources :drinks
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :drinks
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+end
+end
